e83974c739ac19a15e689e1ed7c8d2a20b999422
sleontyev <sleontyev@ozon.ru>
2022-09-29 18:03:59 +0300

    base concepts

---
+++ variables/src/main.rs
@@ -0,0 +1,33 @@
+use std::io;
+
+fn main() {
+    let mut x = 5;
+    println!("{}", x);
+    
+    x = 6;
+    println!("{}", x);
+
+    index();
+}
+
+
+fn index() {
+    let a = [1, 2, 3, 4, 5];
+
+    println!("Please enter an array index.");
+
+    let mut index = String::new();
+
+    io::stdin()
+        .read_line(&mut index)
+        .expect("Failed to read line");
+
+    let index: usize = index
+        .trim()
+        .parse()
+        .expect("Index entered was not a number");
+
+    let element = a[index];
+
+    println!("The value of the element at index {index} is: {element}");
+}
